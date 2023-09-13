2a73a43c4b83e2d892d54ba2e871651d32bb3695
Rafiulsam <rafiulsam69@gmail.com>
2023-09-14 03:07:44 +0600

    added meals card

--- src/Components/Meal/Meal.jsx
+++ src/Components/Meal/Meal.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './Meal.css'
 const Meal = (props) => {
-   const {strMeal} = props.meal
+    console.log(props.meal);
+    const { strMeal, strMealThumb, strTags } = props.meal
     return (
-        <div>
-            <h1>Name: {strMeal}</h1>
+        <div className='meal'>
+            <img src={strMealThumb} alt="" />
+            <div style={{ padding: "0 20px" }}>
+                <h2>{strMeal}</h2>
+                <p>Price: $$$</p>
+                <p>Category: {strTags ? strTags :"Unknown" }</p>
+            </div>
+                <button className='btn-cart'>Add to Cart</button>
         </div>
     );
 };
--- src/Components/Meals/Meals.jsx
+++ src/Components/Meals/Meals.jsx
@@ -2,20 +2,22 @@ import React, { useEffect, useState } from 'react';
 import './Meals.css'
 import Meal from '../Meal/Meal';
 const Meals = () => {
-        const [meals, setMeals] = useState([])
+    const [meals, setMeals] = useState([])
 
-        useEffect(()=>{
-            const url ='https://www.themealdb.com/api/json/v1/1/search.php?s'
-            fetch(url)
+    useEffect(() => {
+        const url = 'https://www.themealdb.com/api/json/v1/1/search.php?s'
+        fetch(url)
             .then(res => res.json())
-            .then(data=> setMeals(data.meals))
-        },[])
+            .then(data => setMeals(data.meals))
+    }, [])
     return (
-        <div>
-            <h3>Meal Found: {meals.length}</h3>
+        <div className='meals'>
+                <h3 style={{textAlign:'center'}}>Meal Found: {meals.length}</h3>
+            <div className='meal-container'>
                 {
                     meals.map(meal => <Meal key={meal.idMeal} meal={meal}></Meal>)
                 }
+            </div>
         </div>
     );
 };
